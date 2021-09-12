d0936b978921d7460d34c5a60af5318c0cb18012
danielvm-git <danielvm@gmail.com>
2021-09-12 15:22:29 -0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -91,10 +91,10 @@ def ajaxfile():
         return jsonify(response)
 
 # * ###########################################################################
-# * ROUTE TO WEBHOOK
+# * ROUTE TO WEBHOOK TEST
 # * ###########################################################################
-@app.route('/webhook', methods=['POST'])
-def webhook():
+@app.route('/webhooktest', methods=['POST'])
+def webhooktest():
         
     data = {}
     try:
@@ -121,38 +121,119 @@ def webhook():
         if signal == "ENTRY LONG":
 
             sl_percent = round((1 - (stop_price / entry_price)), 4)
-            portion_size = calculate_client.portion_size(
-                usdt_balance, sl_percent)
+            portion_size = calculate_client.portion_size(usdt_balance, sl_percent)
 
-            quantity = calculate_client.convert_portion_size_to_quantity(
-                coin_pair, portion_size)
+            quantity = calculate_client.convert_portion_size_to_quantity(coin_pair, portion_size)
             side = "BUY"
-            order_response = calculate_client.order(side, quantity, coin_pair, interval, portion_size, stop_price)
+            order_response = calculate_client.long_order(side, quantity, coin_pair, interval, portion_size, stop_price, sl_percent)
 
         elif signal == "ENTRY SHORT":
 
             sl_percent = round(((stop_price / entry_price) - 1), 4)
-            portion_size = calculate_client.portion_size(
-                usdt_balance, sl_percent)
-
-            quantity = calculate_client.convert_portion_size_to_quantity(
-                coin_pair, portion_size)
-
-            logger.error("SL ", sl_percent, "portionS ", portion_size, "Q ", quantity)
+            portion_size = calculate_client.portion_size(usdt_balance, sl_percent)
+            quantity = calculate_client.convert_portion_size_to_quantity(coin_pair, portion_size)
             side = "SELL"
-            order_response = calculate_client.short_order(side, quantity, coin_pair, interval, portion_size, stop_price)
+            order_response = calculate_client.short_order(side, quantity, coin_pair, interval, portion_size, stop_price, sl_percent)
 
     elif signal == "EXIT LONG":
         side = "SELL"
         quantity = 0
         portion_size = 0
-        order_response = calculate_client.order(side, quantity, coin_pair, interval, portion_size, stop_price)
+        order_response = calculate_client.long_order(side, quantity, coin_pair, interval, portion_size, stop_price, sl_percentage=0)
+
 
     elif signal == "EXIT SHORT":
         side = "BUY"
         quantity = 0
         portion_size = 0
-        order_response = calculate_client.short_order(side, quantity, coin_pair, interval, portion_size, stop_price)
+        order_response = calculate_client.short_order(side, quantity, coin_pair, interval, portion_size, stop_price, sl_percentage=0)
+    else:
+        return "An error occured, can read the signal"
+
+    if order_response:
+        return {
+            "code": "success",
+            "message": "order executed"
+        }
+    else:
+        logger.error("order failed")
+
+        return {
+            "code": "error",
+            "message": "order failed"
+        }
+
+# * ###########################################################################
+# * ROUTE TO WEBHOOK
+# * ###########################################################################
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = {}
+    try:
+        data = json.loads(request.data)
+    except Exception as e:
+        logger.error("an exception occured - {}".format(e))
+        return {"code": "error",
+                "message": "Unable to read webhook"}
+
+    if data['password'] != config_client.PASSWORD:
+        return {
+            "code": "error",
+            "message": "Nice try, invalid passphrase"
+        }
+
+    interval = data['interval']
+    coin_pair = data['ticker']
+    signal = data['signal']
+    stop_price = 0
+    if signal == "ENTRY LONG" or signal == "ENTRY SHORT":
+        stop_price = data['stopprice']
+        entry_price = data['entryprice']
+        usdt_balance = exchange_client.get_usdt_balance()
+        if signal == "ENTRY LONG":
+
+            # TODO entry_long - preciso quantity e symbol
+            sl_percent = round((1 - (stop_price / entry_price)), 4)
+            portion_size = exchange_client.portion_size(usdt_balance, sl_percent)
+            quantity = exchange_client.convert_portion_size_to_quantity(coin_pair, portion_size)
+            rate_steps, quantity_steps = exchange_client.get_tick_and_step_size(coin_pair)
+            quantity = exchange_client.rounding_exact_quantity(quantity, quantity_steps)
+
+            trigger_condition = stop_price * 1.02
+            stop_price = exchange_client.rounding_exact_quantity(stop_price, rate_steps)
+            trigger_condition = exchange_client.rounding_exact_quantity(trigger_condition, rate_steps)
+            order_response = exchange_client.long_order(quantity,coin_pair)
+            exchange_client.set_long_stop_loss(coin_pair,quantity,stop_price,trigger_condition)
+
+        elif signal == "ENTRY SHORT":
+
+            # TODO entry_short
+            sl_percent = round(((stop_price / entry_price) - 1), 4)
+            portion_size = exchange_client.portion_size(usdt_balance, sl_percent)
+            quantity = exchange_client.convert_portion_size_to_quantity(coin_pair, portion_size)
+            rate_steps, quantity_steps = exchange_client.get_tick_and_step_size(coin_pair)
+            quantity = exchange_client.rounding_exact_quantity(quantity, quantity_steps)
+
+            trigger_condition = stop_price * 0.98
+            stop_price = exchange_client.rounding_exact_quantity(stop_price, rate_steps)
+            trigger_condition = exchange_client.rounding_exact_quantity(trigger_condition, rate_steps)
+            order_response = exchange_client.long_order(quantity,coin_pair)
+            exchange_client.set_short_stop_loss(coin_pair,quantity,stop_price,trigger_condition)
+
+    elif signal == "EXIT LONG":
+
+        # TODO exit_long
+        quantity = exchange_client.check_is_sl_hit(coin_pair)
+        order_response = exchange_client.exit_long(coin_pair,quantity)
+        logger.debug("DEBUG")
+
+    elif signal == "EXIT SHORT":
+        
+        # TODO exit_short
+        quantity = exchange_client.check_is_sl_hit(coin_pair)
+        order_response = exchange_client.exit_short(coin_pair,quantity)
+        logger.debug("DEBUG")
+
     else:
         return "An error occured, can read the signal"
 
